7866a9135bc99f0cc7240282b1983e3f32ed8118
james Agu <preagu@yahoo.com>
2023-10-04 18:01:51 +0100

    Created Counter functionality using class based component.

---
+++ src/Counter.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  increase = () => {
+    this.setState((oldState) => {
+      return {count: oldState.count + 1};
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>You clicked {this.state.count} times</p>
+        <button onClick={this.increase}>Click me</button>
+      </div>
+    )
+  }
+}
